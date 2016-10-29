e5ac358946d73645062985292645a1c81debebfb
Bryan MacDiarmid <bryanmacdiarmid@bryan.local>
2016-10-29 14:13:01 -0400

    Adds source maps for browser and specs

--- webpack.config-test.js
+++ webpack.config-test.js
@@ -9,5 +9,6 @@ module.exports = {
       exclude: /node_modules/,
       loader: "babel-loader"
     }]
-  }
+  },
+  devtool: "cheap-module-source-map" // faster than 'source-map'
 };
--- webpack.config.js
+++ webpack.config.js
@@ -13,5 +13,6 @@ module.exports = {
       exclude: /node_modules/,
       loader: "babel-loader"
     }]
-  }
+  },
+  devtool: "eval-source-map"
 };
